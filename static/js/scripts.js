b58977fd105cf96747634322e9f586cde417e3d3
Maria Lobareva <MariaLobareva@yandex.ru>
2017-11-26 01:54:13 +0300

    добавила функционал кнопки Обновить функция печати постов теперь в jquery

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -68,6 +68,19 @@ function placeTags(result){
     }
 }
 
+function rankCode(btn_id){
+  switch(btn_id){
+    case 'pos':
+      return 1;
+    case 'neg':
+      return -1;
+    case 'any':
+      return 0;
+    case 'all':
+      return 2;
+  }
+}
+
 function setReviewColor(rank){
   switch(rank){
     case -1:
@@ -80,60 +93,81 @@ function setReviewColor(rank){
       return -1;
   }
 }
-/*
-function filterReviews(id){
-  switch(id){
-    case 'all':
-      placeReviews();
-    case 'pos':
-    case 'neg':
-    case 'any':
-  }
-}
-*/
 
-function placeReviews(result, mode = 0){
-  if(mode == 0)
-    var reviewsArr = JSON.parse(result);
+function postToHTML(){}
+
+function placeReviews(result, btn_id){
+  var reviewsArr = JSON.parse(result);
   var str = "";
-  /*
-  reviewsArr[1] - link
-  2 - text
-  3 - {-1(neg), 0(pos), 1(any), -100(spam), -200(ad)}
-  4 - date
-  6 - photolink
-  7 - social_id
-  8 - firm_id
-  10 - name
-  */
+  var color;
 
   var feedbacks = document.getElementById("lenta");
   while(feedbacks.firstChild){
     feedbacks.removeChild(feedbacks.firstChild);
   }
 
-  for(var i = 0; i<reviewsArr.length; i++){
-    var color;
-    if(color = setReviewColor(reviewsArr[i].rank)==-1)
-      break;
+  for(var i = 0; i<reviewsArr.length, rankCode(btn_id)>=-1&&rankCode(btn_id)<=2; i++){
+    if(rankCode(btn_id)>=-1&&rankCode(btn_id)<=1){
+      color = setReviewColor(reviewsArr[i].rank)
 
-    str += '<div class = "fb_head row"><div class = "col-sm-6 social_nw row">';
-    str += '<img src = "'+reviewsArr[i].photolink+'" id = "avatar">';
-    str += '<p id = "username">'+reviewsArr[i].name+'</p>';
-    str += '<button type="button" class="btn btn-lg btn-link"><p id = "social_nw_name">'+reviewsArr[i].social_id+'</p></button>';
-    str += '<p id = "feedback_date">'+reviewsArr[i].date+'</p></div>';
-    str += '<div class = "col-sm-6 mistake_btn">';
-    str += '<button type="button" class="btn btn-link dropdown-toggle" data-toggle="dropdown"><span class="caret" float="right">Ошибка</span></button>';
-    str += '<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu3"><li role="presentation"><a role="menuitem" href="#">Положительный</a></li>';
-    str += '<li role="presentation"><a role="menuitem" href="#">Отрицательный</a></li><li role="presentation"><a role="menuitem" href="#">Неопределенный</a></li></ul></div>';
-    str += '</div><div class = "feedback_text"><p id = "fb_text">'+ reviewsArr[i].text +'</p></div>';
+      str += '<div class = "fb_head row"><div class = "col-sm-6 social_nw row">';
+      str += '<img src = "'+reviewsArr[i].photolink+'" id = "avatar">';
+      str += '<p id = "username">'+reviewsArr[i].name+'</p>';
+      str += '<button type="button" class="btn btn-lg btn-link"><p id = "social_nw_name">'+reviewsArr[i].social_id+'</p></button>';
+      str += '<p id = "feedback_date">'+reviewsArr[i].date+'</p></div>';
+      str += '<div class = "col-sm-6 mistake_btn">';
+      str += '<button type="button" class="btn btn-link dropdown-toggle" data-toggle="dropdown"><span class="caret" float="right">Ошибка</span></button>';
+      str += '<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu3"><li role="presentation"><a role="menuitem" href="#">Положительный</a></li>';
+      str += '<li role="presentation"><a role="menuitem" href="#">Отрицательный</a></li><li role="presentation"><a role="menuitem" href="#">Неопределенный</a></li></ul></div>';
+      str += '</div><div class = "feedback_text"><p id = "fb_text">'+ reviewsArr[i].text +'</p>';
+      if(reviewsArr[i].img!="")
+        str += '<img src = "'+ reviewsArr[i].img + '">';
+      str += '</div>';
 
-    var curPost = document.createElement ('div');
-    curPost.className("feedback");
-    curPost.style.background-color = color;
-    curPost.innerHTML(str);
+      var curPost = document.createElement ('div');
+      curPost.className("feedback");
+      curPost.style.background-color = color;
+      curPost.innerHTML(str);
+    }
   }
 }
+
+$(document).ready(function() {
+  $('#upd').click(function() {
+    var firm_id = $('.btnChooseFirm').hasClass('selectedButton').attr('data-id');
+    $.ajax({
+      url: 'http://52.173.83.176/getReviewsData/?firm_id='+firm_id,
+      data: { 'firm_id': firm_id},
+      datatype: 'json',
+      success: function (result) {
+         placeReviews(result, 2);
+      },
+     error: function() {
+         alert("Error");
+         return;
+     }
+    });
+  });
+})
+
+$(document).ready(function() {
+  $('.btn btn-default').click(function() {
+    var btn_id = $(this.attr('id'));
+    var firm_id = $('.btnChooseFirm').hasClass('selectedButton');
+    $.ajax({
+      url: 'http://52.173.83.176/getReviewsData/?firm_id='+firm_id,
+      data: { 'firm_id': firm_id, 'btn_id': btn_id},
+      datatype: 'json',
+      success: function (result, btn_id ) {
+         placeReviews(result, btn_id);
+      },
+     error: function() {
+         alert("Error");
+         return;
+     }
+    });
+  });
+})
 
 $(document).ready(function() {
    $('.btnChooseFirm').click(function() {
@@ -146,18 +180,6 @@ $(document).ready(function() {
       datatype: 'json',
       success: function (result) {
          placeTags(result);
-      },
-     error: function() {
-         alert("Error");
-         return;
-     }
-   });
-   $.ajax({
-      url: '',
-      data: { 'firm_id': firm_id },
-      datatype: 'json',
-      success: function (result) {
-         placeReviews(result);
       },
      error: function() {
          alert("Error");
